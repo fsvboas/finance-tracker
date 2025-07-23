9328ac339ed34c158e771b1ea1eec52675fe6ea8
fsvboas <fsvboas.dev@gmail.com>
2025-07-23 16:19:14 -0300

    feat: create financial-summary-card component

---
+++ app/components/financial-summary-card.tsx
@@ -0,0 +1,30 @@
+import { Banknote, BanknoteArrowDown, BanknoteArrowUp } from "lucide-react";
+import CurrencyFormat from "../helpers/currency-format";
+import Column from "./utils/column";
+import Row from "./utils/row";
+
+interface CardProps {
+  title: "Entradas" | "Saídas" | "Total";
+  value?: number | string;
+}
+const Card = ({ title, value }: CardProps) => {
+  return (
+    <Column className="p-4 bg-white w-full max-w-60 min-h-32 justify-between rounded">
+      <Row className="justify-between">
+        <p>{title}</p>
+        <span>{cardIconMap[title]}</span>
+      </Row>
+      <p className="text-3xl font-medium">
+        <CurrencyFormat>{value}</CurrencyFormat>
+      </p>
+    </Column>
+  );
+};
+
+export default Card;
+
+const cardIconMap = {
+  Entradas: <BanknoteArrowUp className="text-green-600" />,
+  Saídas: <BanknoteArrowDown className="text-red-600" />,
+  Total: <Banknote />,
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Card from "./components/financial-summary-card";
+import Column from "./components/utils/column";
+import Flex from "./components/utils/flex";
+
 export default function Home() {
+  const incomings = 100000;
+  const outcomings = 50000;
+  const total = incomings - outcomings;
+
   return (
     <div className="min-h-screen w-full flex items-center bg-black/95 justify-center">
-      <h1 className="text-2xl text-white">Finance Tracker</h1>
+      <Column className="items-center space-y-4 w-full">
+        <h1 className="text-2xl text-white">Finance Tracker</h1>
+        <Flex className="sm:space-x-2 max-sm:space-y-2 flex-col sm:flex-row w-full justify-center items-center px-2">
+          <Card title="Entradas" value={incomings} />
+          <Card title="Saídas" value={outcomings} />
+          <Card title="Total" value={total} />
+        </Flex>
+      </Column>
     </div>
   );
 }
