f318361c8b9f4bea953881736238e89e9d86f51d
fsvboas <fsvboas.dev@gmail.com>
2025-07-24 01:15:07 -0300

    feat: create add-new-transaction-dialog layout

--- app/components/add-new-transaction-dialog.tsx
+++ app/components/add-new-transaction-dialog.tsx
@@ -1,14 +1,19 @@
 import { Button } from "./ui/button";
+import { DatePicker } from "./ui/date-picker";
 import {
   Dialog,
   DialogClose,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "./ui/dialog";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import Column from "./utils/column";
+import Flex from "./utils/flex";
 
 interface AddNewTransactionDialogProps {
   trigger: React.ReactNode;
@@ -20,16 +25,42 @@ const AddNewTransactionDialog = ({ trigger }: AddNewTransactionDialogProps) => {
       <DialogTrigger asChild>{trigger}</DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Nova Transação</DialogTitle>
-          <DialogDescription>Dialog teste</DialogDescription>
+          <DialogTitle />
+          <Tabs defaultValue="incoming" className="items-center">
+            <TabsList className="w-[80%] sm:w-1/2">
+              <TabsTrigger
+                className="text-green-600 hover:cursor-pointer"
+                value="incoming"
+              >
+                Entrada
+              </TabsTrigger>
+              <TabsTrigger
+                className="text-red-600 hover:cursor-pointer"
+                value="outcoming"
+              >
+                Saída
+              </TabsTrigger>
+            </TabsList>
+          </Tabs>
         </DialogHeader>
+        <Column className="space-y-2">
+          <Label htmlFor="transaction">Transação</Label>
+          <Input id="transaction" />
+        </Column>
+        <Flex className="sm:space-x-2 max-sm:flex-col max-sm:space-y-4">
+          <Column className="space-y-2">
+            <Label htmlFor="value">Valor</Label>
+            <Input id="value" />
+          </Column>
+          <DatePicker />
+        </Flex>
         <DialogFooter>
-          <Button className="hover:cursor-pointer">Adicionar</Button>
           <DialogClose asChild>
             <Button className="hover:cursor-pointer" variant="secondary">
               Cancelar
             </Button>
           </DialogClose>
+          <Button className="hover:cursor-pointer">Adicionar</Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
