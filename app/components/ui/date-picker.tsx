2ff4e49a3788a52f226758258be99ea1a82b6656
fsvboas <fsvboas.dev@gmail.com>
2025-07-24 01:13:41 -0300

    feat: create date-picker component

---
+++ app/components/ui/date-picker.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { ChevronDownIcon } from "lucide-react";
+import { useState } from "react";
+import Column from "../utils/column";
+import Flex from "../utils/flex";
+import { Button } from "./button";
+import { Calendar } from "./calendar";
+import { Input } from "./input";
+import { Label } from "./label";
+import { Popover, PopoverContent, PopoverTrigger } from "./popover";
+
+function DatePicker() {
+  const [open, setOpen] = useState<boolean>(false);
+  const [date, setDate] = useState<Date | undefined>(undefined);
+
+  return (
+    <Flex className="space-x-2 max-sm:flex-col max-sm:space-y-4">
+      <Column className="flex flex-col space-y-2 w-full">
+        <Label htmlFor="date-picker">Data</Label>
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              variant="outline"
+              id="date-picker"
+              className="w-full min-w-[150px] justify-between font-normal"
+            >
+              {date ? date.toLocaleDateString() : "Selecione a data"}
+              <ChevronDownIcon />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="overflow-hidden p-0" align="start">
+            <Calendar
+              mode="single"
+              selected={date}
+              captionLayout="dropdown"
+              onSelect={(date) => {
+                setDate(date);
+                setOpen(false);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </Column>
+      <Column className="space-y-2">
+        <Label htmlFor="time-picker">Horário</Label>
+        <Input
+          type="time"
+          id="time-picker"
+          step="1"
+          defaultValue="10:30:00"
+          className="bg-background w-full appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+        />
+      </Column>
+    </Flex>
+  );
+}
+
+export { DatePicker };
