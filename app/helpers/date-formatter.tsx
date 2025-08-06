8be1c052297f5e2c75482a5370c9a321e883f65c
fsvboas <fsvboas.dev@gmail.com>
2025-08-06 18:38:07 -0300

    feat: create date-formatter helper

---
+++ app/helpers/date-formatter.tsx
@@ -0,0 +1,18 @@
+import { format, isValid, parseISO } from "date-fns";
+
+interface DateFormatterProps {
+  children: string;
+}
+
+const dateFormatter = (dateAsString: string) => {
+  const date = parseISO(dateAsString);
+  return isValid(date) ? format(date, "dd/MM/yyyy") : undefined;
+};
+
+const DateFormatter = ({ children }: DateFormatterProps) => {
+  const date = parseISO(children);
+  if (!isValid(date)) return null;
+  return <>{dateFormatter(children)}</>;
+};
+
+export default DateFormatter;
