e6e2a60c1bea04234c035aeff9f53787d8ece3e4
fsvboas <fsvboas.dev@gmail.com>
2025-08-11 16:33:19 -0300

    chore: setup tanstack-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TanstackQueryProvider } from "./libs/tanstack-query";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt">
-      <body className={`${inter} antialiased`}>{children}</body>
+      <body className={`${inter} antialiased`}>
+        <TanstackQueryProvider>{children}</TanstackQueryProvider>
+      </body>
     </html>
   );
 }
