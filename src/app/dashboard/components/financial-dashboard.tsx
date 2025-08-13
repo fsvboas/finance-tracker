be44aa33373405a5132f758dc0aae4462d997a57
fsvboas <fsvboas.dev@gmail.com>
2025-08-12 21:14:45 -0300

    refactor: change container width

--- src/app/dashboard/components/financial-dashboard.tsx
+++ src/app/dashboard/components/financial-dashboard.tsx
@@ -52,7 +52,7 @@ export default function FinancialDashboard() {
   }, [filteredTransactions]);
 
   return (
-    <Column className="items-center space-y-2 max-w-7xl mx-auto">
+    <Column className="items-center space-y-2 max-w-5xl mx-auto">
       <TimePeriodSelector
         selectedYear={year}
         setSelectedYear={setYear}
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Row from "./utils/row";
 const Header = () => {
   return (
     <header className="w-full bg-black text-white p-4 fixed top-0 left-0 z-10">
-      <Row className="max-w-7xl mx-auto justify-between">
+      <Row className="max-w-5xl mx-auto justify-between">
         <h1 className="text-lg font-bold">Finance Tracker</h1>
         <UserMenu />
       </Row>
