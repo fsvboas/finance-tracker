86b529976fe47f09f566ba4ffe528c2ceca87268
fsvboas <fsvboas.dev@gmail.com>
2025-08-15 01:34:47 -0300

    refactor: add icon to empty-transaction-history button

--- src/app/dashboard/components/empty-transaction-history.tsx
+++ src/app/dashboard/components/empty-transaction-history.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/src/components/button";
 import Column from "@/src/components/utils/column";
-import { CircleSlash2 } from "lucide-react";
+import { CircleSlash2, ListPlus } from "lucide-react";
 import AddTransactionFormDialog from "./add-transaction-form-dialog";
 
 const EmptyTransactionHistory = () => {
@@ -9,7 +9,12 @@ const EmptyTransactionHistory = () => {
       <CircleSlash2 size={40} />
       <p className="text-center">Nenhuma transação foi adicionada neste mês.</p>
       <AddTransactionFormDialog
-        trigger={<Button className="cursor-pointer">Nova Transação</Button>}
+        trigger={
+          <Button className="cursor-pointer">
+            <ListPlus className="!w-5 !h-5" />
+            Nova Transação
+          </Button>
+        }
       />
     </Column>
   );
