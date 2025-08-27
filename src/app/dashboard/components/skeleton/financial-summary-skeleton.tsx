1c21b400c8db767e73824ce4871a3e42ce56ccb4
fsvboas <fsvboas.dev@gmail.com>
2025-08-26 23:55:09 -0300

    refactor: update financial-summary-skeleton layout

--- src/app/dashboard/components/skeleton/financial-summary-skeleton.tsx
+++ src/app/dashboard/components/skeleton/financial-summary-skeleton.tsx
@@ -3,7 +3,8 @@ import Flex from "@/src/components/utils/flex";
 
 export default function FinancialSummarySkeleton() {
   return (
-    <Flex className="sm:space-x-2 max-sm:space-y-2 flex-col sm:flex-row w-full justify-center items-center">
+    <Flex className="gap-2 w-full justify-center items-center grid grid-cols-2 md:grid-cols-4">
+      <Skeleton className="w-full h-[88px]" />
       <Skeleton className="w-full h-[88px]" />
       <Skeleton className="w-full h-[88px]" />
       <Skeleton className="w-full h-[88px]" />
