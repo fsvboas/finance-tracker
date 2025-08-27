6b92b530d5d01d73c5834e07fe77b2740ac7bfa8
fsvboas <fsvboas.dev@gmail.com>
2025-08-27 14:44:32 -0300

    refactor: update delete-account-dialog to be a drawer when in mobile devices

---
+++ src/app/minha-conta/components/delete-account-dialog-drawer.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { Button } from "@/src/components/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/src/components/dialog";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/src/components/drawer";
+import Column from "@/src/components/utils/column";
+import { useMediaQuery } from "@/src/hooks/use-media-query";
+import { useState } from "react";
+import DeleteAccountForm from "./delete-account-form";
+
+interface DeleteAccountDialogDrawerProps {
+  trigger: React.ReactNode;
+}
+
+const DeleteAccountDialogDrawer = ({
+  trigger,
+}: DeleteAccountDialogDrawerProps) => {
+  const isDesktop = useMediaQuery("(min-width: 768px)");
+
+  const [open, setOpen] = useState<boolean>(false);
+
+  if (isDesktop) {
+    return (
+      <Dialog
+        open={open}
+        onOpenChange={(isOpen) => {
+          setOpen(isOpen);
+        }}
+      >
+        <DialogTrigger asChild>{trigger}</DialogTrigger>
+        <DialogContent onOpenAutoFocus={(e) => e.preventDefault()}>
+          <DialogHeader className="overflow-hidden border-b-1 border-black/10">
+            <DialogTitle className="overflow-hidden text-ellipsis pb-2">
+              Excluir Conta
+            </DialogTitle>
+          </DialogHeader>
+          <Column className="space-y-4">
+            <p>
+              Ao excluir sua conta na plataforma Finance Tracker, todos os seus
+              dados serão removidos do nosso sistema.
+            </p>
+            {/* <p>
+            Recomendamos que faça o backup de seus dados antes da exclusão,
+            utilizando o botão &quot;Baixar Planilha&quot;.
+          </p> */}
+            <DeleteAccountForm />
+          </Column>
+        </DialogContent>
+      </Dialog>
+    );
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>{trigger}</DrawerTrigger>
+      <DrawerContent className="p-4">
+        <DrawerHeader className="space-y-2 p-4">
+          <DrawerTitle>Excluir Conta</DrawerTitle>
+          <DrawerDescription>
+            Ao excluir sua conta na plataforma Finance Tracker, todos os seus
+            dados serão removidos do nosso sistema.
+          </DrawerDescription>
+        </DrawerHeader>
+        <DeleteAccountForm />
+        <DrawerFooter className="w-full mt-2">
+          <DrawerClose asChild className="w-full">
+            <Button variant="outline" className="w-full">
+              Cancelar
+            </Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default DeleteAccountDialogDrawer;
--- src/app/minha-conta/components/user-data-section.tsx
+++ src/app/minha-conta/components/user-data-section.tsx
@@ -5,7 +5,7 @@ import Column from "@/src/components/utils/column";
 import Row from "@/src/components/utils/row";
 import Show from "@/src/components/utils/show";
 import { Database, Loader2Icon, UserRoundX } from "lucide-react";
-import DeleteAccountDialog from "./delete-account-dialog";
+import DeleteAccountDialogDrawer from "./delete-account-dialog-drawer";
 
 const UserDataSection = () => {
   return (
@@ -34,7 +34,7 @@ const UserDataSection = () => {
             Não quer mais utilizar o Finance Tracker?
           </dt>
           <dd className="w-full sm:w-[150px]">
-            <DeleteAccountDialog
+            <DeleteAccountDialogDrawer
               trigger={
                 <Button className="cursor-pointer bg-red-500 hover:bg-red-400 duration-300 text-white w-full">
                   <Show when={false} fallback={<UserRoundX />}>
