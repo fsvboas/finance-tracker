cfd73e61cdf63cd95f83a7bfefde220df475cf42
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2026-01-08 02:53:27 -0300

    chore: add some configs to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     globalNotFound: true,
+    optimizeCss: true,
   },
+  modularizeImports: {
+    "lucide-react": {
+      transform: "lucide-react/dist/esm/icons/{{kebabCase member}}",
+    },
+    "date-fns": {
+      transform: "date-fns/{{member}}",
+    },
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  compress: true,
 };
 
 export default nextConfig;
