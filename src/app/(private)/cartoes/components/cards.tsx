bd3495f64f490a2d640c591634da2a03096c55fc
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2026-01-08 02:37:03 -0300

    feat: create cards component

---
+++ src/app/(private)/cartoes/components/cards.tsx
@@ -0,0 +1,22 @@
+import Card from "@/src/app/(private)/cartoes/components/card";
+import { getCards } from "@/src/app/(private)/cartoes/services";
+import { useQuery } from "@tanstack/react-query";
+
+const Cards = () => {
+  const { data, isPending: pendingGetCards } = useQuery({
+    queryFn: getCards,
+    queryKey: ["cards"],
+  });
+
+  const cards = data || [];
+
+  return (
+    <div className="w-full grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-2">
+      {cards.map((card, index) => (
+        <Card key={index} card={card} />
+      ))}
+    </div>
+  );
+};
+
+export default Cards;
--- src/app/(private)/cartoes/page.tsx
+++ src/app/(private)/cartoes/page.tsx
@@ -1,10 +1,6 @@
-"use client";
-
-import Card from "@/src/app/(private)/cartoes/components/card";
+import Cards from "@/src/app/(private)/cartoes/components/cards";
 import CardsScreenHeader from "@/src/app/(private)/cartoes/components/cards-screen-header";
-import { getCards } from "@/src/app/(private)/cartoes/services";
 import { Container } from "@/src/components/core/container";
-import { useQuery } from "@tanstack/react-query";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,21 +8,10 @@ export const metadata: Metadata = {
 };
 
 export default function CardsScreen() {
-  const { data, isPending: pendingGetCards } = useQuery({
-    queryFn: getCards,
-    queryKey: ["cards"],
-  });
-
-  const cards = data || [];
-
   return (
     <Container variant="page">
       <CardsScreenHeader />
-      <div className="w-full grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 mt-2">
-        {cards.map((card, index) => (
-          <Card key={index} card={card} />
-        ))}
-      </div>
+      <Cards />
     </Container>
   );
 }
