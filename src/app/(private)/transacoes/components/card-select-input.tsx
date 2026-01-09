c269a0e582c831d4a0915a06c9f19a431377f472
Felippe Vilas Boas <fsvboas.dev@gmail.com>
2026-01-08 21:00:10 -0300

    refactor: replace card combobox-input by select-input

---
+++ src/app/(private)/transacoes/components/card-select-input.tsx
@@ -0,0 +1,43 @@
+import { getCards } from "@/src/app/(private)/cartoes/services";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/src/components/core/select";
+import { useQuery } from "@tanstack/react-query";
+
+interface CardSelectInputProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const CardSelectInput = ({ value, onChange }: CardSelectInputProps) => {
+  const { data, isPending: pendingGetCards } = useQuery({
+    queryFn: getCards,
+    queryKey: ["cards"],
+  });
+
+  const cards = data || [];
+
+  return (
+    <Select value={value} onValueChange={onChange}>
+      <SelectTrigger className="w-full text-base">
+        <SelectValue placeholder="Selecione o cartão" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          {cards?.map((card) => (
+            <SelectItem key={card?.id} value={card?.name}>
+              {card?.name}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default CardSelectInput;
